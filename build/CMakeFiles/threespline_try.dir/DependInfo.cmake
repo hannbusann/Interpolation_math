6a382da66dd810cde74e7cfbf87472336823961f
hannbusann <1617126946@qq.com>
2018-10-15 22:17:35 +0800

    susumuteimasu

---
+++ build/CMakeFiles/threespline_try.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zjudancer/try/threespline/test/threespline_test.cpp" "/home/zjudancer/try/threespline/build/CMakeFiles/threespline_try.dir/test/threespline_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  ".././include/Utility"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- include/ThreeSpline/SimpleInterpolation.hpp
+++ include/ThreeSpline/SimpleInterpolation.hpp
@@ -1,13 +1,25 @@
-#pragma once	
-#include <Eigen/Dense>
-#include <>
+#pragma once
 #include <iostream>
-
-namespace Motion{
-	class DAinterpolation{
-	public:
+#include <Eigen/Dense>
+#include "Polynomial.hpp"
 
 
-	}
+namespace Motion
+{
+
+
+class   DAinterpolation
+{
+public:
+
+    DAinterpolation(const double& x_array, double& y_arrary, double& v_arrary)
+    {}
+
+private:
+    Polynomial<3>* Poly;
+    float m_x = 
+
+
+}
 
 }

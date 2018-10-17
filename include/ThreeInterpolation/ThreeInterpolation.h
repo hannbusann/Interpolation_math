615d4803ebb998c8f99cfef6342d1535b79c9bab
FW <1617126946@qq.com>
2018-10-17 21:03:30 +0800

    total new interpolation to master

---
+++ include/ThreeInterpolation/ThreeInterpolation.h
@@ -0,0 +1,24 @@
+//
+// Created by apple on 18/10/17.
+// E-mail: zjufanwu@zju.edu.cn
+
+#ifndef THREEINTERPOLATION_H
+#define THREEINTERPOLATION_H
+
+#include <Eigen/Dense>
+#include <EigenTypes.h>
+#include <Polynomial.hpp>
+
+namespace dmotion {
+
+    class ThreeInterpolation {
+    public:
+        Polynomial *poly_ptr_;
+        vector<>
+
+    };
+
+}
+
+
+#endif //THREESPLINE_TRY_THREEINTERPOLATION_H
